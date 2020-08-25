5f67ce2964b47b79ca9bb83c9c030a44dcc27126
victoria <kerza@student.douglascollege.ca>
2020-08-25 12:20:07 -0700

    1

--- Website.py
+++ Website.py
@@ -11,9 +11,25 @@ app.permanent_session_lifetime = timedelta(days=5)
 
 db = SQLAlchemy(app)
 
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/view")
+def view():
+    return render_template("view.html", values=users.query.all())
 
 
 @app.route("/login", methods=["POST", "GET"])
@@ -22,12 +38,22 @@ def login():
         session.permanent = True
         user = request.form["nm"]
         session["user"] = user
+
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = users(user, "")
+            db.session.add(usr)
+            db.session.commit()
+
         flash(f"Hi, {user}.", "info")
         return redirect(url_for("user"))
     else:
         if "user" in session:
             flash("You are already logged in!", "info")
             return redirect(url_for("user"))
+
         return render_template("login.html")
 
 
@@ -39,6 +65,9 @@ def user():
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
             flash("Email was saved", "info")
         else:
             if "email" in session:
@@ -68,10 +97,16 @@ def scheduling():
     return render_template("scheduling.html")
 
 
+@app.route("/index/")
+def index():
+    return render_template("index.html")
+
+
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
